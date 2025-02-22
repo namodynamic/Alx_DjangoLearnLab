cca52ca589a6e689c1eef6ed6ab8644d270c9f29
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-02-23 00:21:55 +0100

    Rename view functions for admin, librarian, and member

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import LibraryDetailView
 from .views import list_books
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import admin, librarian, member
+from .views import admin_view, librarian_view, member_view
 
 from . import views
 
@@ -14,7 +14,7 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('admin/', admin, name='admin'),
-    path('librarian/', librarian, name='librarian'),
-    path('member/', member, name='member'),
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
 ]

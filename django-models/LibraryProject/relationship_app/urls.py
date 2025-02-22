f42e136e8dbfc39f88d37afe23f6632a5564d03c
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-02-22 10:45:40 +0100

    Update login and logout paths to use Django's built-in authentication views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import LibraryDetailView
 from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 
@@ -9,6 +10,6 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('register/', views.register_user, name='register'),
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]

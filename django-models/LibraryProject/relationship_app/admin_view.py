bb54fbb8f123451b4e86151373dbe963b1703859
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-02-25 10:47:54 +0100

    Add login requirement to admin dashboard view and improve admin role check

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == "Admin"
 
+@login_required
 @user_passes_test(is_admin)
 def admin_dashboard(request):
     return render(request, 'relationship_app/admin_dashboard.html')

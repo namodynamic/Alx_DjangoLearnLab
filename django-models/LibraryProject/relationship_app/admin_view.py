561d68976a639bbc8df03d6f99b68361d3b28b87
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-02-25 17:08:42 +0100

    Refactor admin view to remove login_required decorator and use a lambda function for user role check

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import user_passes_test
 
 
-@login_required
 @user_passes_test(lambda user: user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin')
 def admin_view(request):
     return render(request, 'relationship_app/admin_dashboard.html')

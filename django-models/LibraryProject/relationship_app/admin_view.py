36bd2dc18cc1b64ac82c62921c6e2facc3ee6579
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-02-25 17:13:41 +0100

    Refactor admin view permission check to use a dedicated is_admin function

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
 
-@user_passes_test(lambda user: user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin')
+@user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_dashboard.html')

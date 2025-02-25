65fb73e2ac5ef6f3f8fb78c4d98d786d7afc34a7
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-02-25 17:05:54 +0100

    Refactor admin view permission check to use a lambda function

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
 
-def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 @login_required
-@user_passes_test(is_admin)
+@user_passes_test(lambda user: user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin')
 def admin_view(request):
     return render(request, 'relationship_app/admin_dashboard.html')

75f46030559545d9ad78a8ff9a3b1829db55a5de
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-02-25 17:33:59 +0100

    Fix is_librarian function to check for 'Librarians' role instead of 'Librarian'

--- django-models/LibraryProject/relationship_app/librarian_view.py
+++ django-models/LibraryProject/relationship_app/librarian_view.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
 def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
+    return user.userprofile.role == 'Librarians'
 
 
 @user_passes_test(is_librarian)

941bfe6574ec110b8e2db30d6ea70e79088e9e40
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-02-25 16:58:49 +0100

    Refactor book management URL paths for consistency

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('admin-dashboard/', admin_view, name='admin_view'),
     path('librarian-dashboard/', librarian_dashboard, name='librarian_view'),
     path('member-dashboard/', member_dashboard, name='member_view'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
     path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
 ]

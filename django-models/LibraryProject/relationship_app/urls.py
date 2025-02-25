6a24b23f90f25fbfc9235eab92ee12ed2b16f524
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-02-25 14:20:33 +0100

    Add login requirement to admin view and refactor URL paths

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import LibraryDetailView
 from .views import list_books
 from django.contrib.auth.views import LoginView, LogoutView
-from .admin_view import admin_dashboard
+from .admin_view import admin_view
 from .librarian_view import librarian_dashboard
 from .member_view import member_dashboard
 
@@ -16,7 +16,7 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('admin-dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('admin-dashboard/', admin_view, name='admin_dashboard'),
     path('librarian-dashboard/', librarian_dashboard, name='librarian_dashboard'),
     path('member-dashboard/', member_dashboard, name='member_dashboard'),
 ]

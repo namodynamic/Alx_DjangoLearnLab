7f5124d66d9403a2a2d346c44c21e534295755a6
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-03-12 14:59:21 +0100

    Add validation for publication year in BookSerializer

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -7,6 +7,11 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = '__all__'
         
+    def validate_publication_year(self, value):
+        if value > datetime.datetime.now().year:
+            raise serializers.ValidationError("Publication year cannot be in the future")
+        return value    
+        
         
 class AuthorSerializer(serializers.ModelSerializer):
     books = BookSerializer(many=True, read_only=True)

6921f423be04b6eace30791ce212ecc4234eb148
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-03-24 21:10:46 +0100

    Add token generation to user registration and update response structure

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,8 +2,9 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework.validators import UniqueValidator
+from rest_framework.authtoken.models import Token
+from .models import CustomUser
 
-CustomUser = get_user_model()
 
 class CustomUserRegistrationSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
@@ -16,10 +17,11 @@ class CustomUserRegistrationSerializer(serializers.ModelSerializer):
         validators=[validate_password]
     )
     password2 = serializers.CharField(write_only=True, required=True)
+    token = serializers.CharField(read_only=True)
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name', 'bio')
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name', 'bio', 'token')
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -27,10 +29,17 @@ class CustomUserRegistrationSerializer(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
-        validated_data.pop('password2')
-        user = CustomUser.objects.create_user(**validated_data)
+        # Remove password2 before creating user
+        validated_data.pop('password2') 
+        # Create user using create_user method
+        user = get_user_model().objects.create_user(**validated_data)
+        # Create token for the user
+        token = Token.objects.create(user=user)
         return user
-   
+    
+    def get_token(self, obj):
+        token = Token.objects.get(user=obj)
+        return token.key
         
 
 
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,25 +1,19 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
-from django.contrib.auth import get_user_model
 from .serializers import CustomUserRegistrationSerializer, UserProfileSerializer
-from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 
-CustomUser = get_user_model()
 
 class RegisterView(APIView):
     def post(self, request):
         serializer = CustomUserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({
-                'token': token.key,
-                'user': UserProfileSerializer(user).data
-            }, status=status.HTTP_201_CREATED)
+            response_data = serializer.data
+            response_data['user'] = UserProfileSerializer(user).data
+            return Response(response_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     
